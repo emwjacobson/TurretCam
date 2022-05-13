249d6eaad106318cc992c00ce6ca621f47759c4d
Emerson Jacobson <emwjacobson@gmail.com>
2022-05-13 13:03:08 -0700

    Added functions to help connect to wifi, removed sdkconfig

--- main/main.c
+++ main/main.c
@@ -4,11 +4,14 @@
 #include "esp_system.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #include "mqtt_client.h"
-#include "esp_wifi.h"
 
 // Setup configs
 // These should /technically/ be set using `make menuconfig`
@@ -34,8 +37,8 @@
 
 static const char* TAG = "Main";
 
-void mqtt_event_handler() {
-    // TODO
+esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event) {
+    return ESP_OK;
 }
 
 esp_err_t init_mqtt() {
@@ -53,15 +56,21 @@ esp_err_t init_mqtt() {
 esp_err_t init_wifi() {
     esp_err_t err;
 
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_netif_init();
+    err = esp_event_loop_create_default();
+    if (err != ESP_OK) {
+        return err;
+    }
 
-    err = esp_wifi_init(&cfg);
+    wifi_init_config_t init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    err = esp_wifi_init(&init_cfg);
     if (err != ESP_OK) {
         return err;
     }
     ESP_LOGI(TAG, "Wifi Initialized");
 
-    wifi_config_t cfg = {
+    wifi_config_t wifi_cfg = {
         .sta = {
             .ssid = CONFIG_WIFI_SSID,
             .password = CONFIG_WIFI_PASSWORD,
@@ -77,7 +86,7 @@ esp_err_t init_wifi() {
     }
     ESP_LOGI(TAG, "WiFi mode set to station");
 
-    err = esp_wifi_set_config(ESP_IF_WIFI_STA, &cfg);
+    err = esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_cfg);
     if (err != ESP_OK) {
         return err;
     }
@@ -87,27 +96,34 @@ esp_err_t init_wifi() {
     if (err != ESP_OK) {
         return err;
     }
-    ESP_LOGI(TAG, "WiFi started");
+    ESP_LOGI(TAG, "WiFi started, connecting to " CONFIG_WIFI_SSID);
 
     return ESP_OK;
 }
 
 void init() {
     esp_err_t err;
+
+    err = nvs_flash_init();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize NVS. Error: %s", esp_err_to_name(err));
+    }
+
     err = init_wifi();
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "Failed to initialize wifi. Error: %s", esp_err_to_name(err));
     }
 
-    // Initialize MQTT
-    err = init_mqtt();
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to initialize MQTT client. Error: %s", esp_err_to_name(err));
-        exit(1);
-    }
+    // // Initialize MQTT
+    // err = init_mqtt();
+    // if (err != ESP_OK) {
+    //     ESP_LOGE(TAG, "Failed to initialize MQTT client. Error: %s", esp_err_to_name(err));
+    //     exit(1);
+    // }
 }
 
 void app_main() {
+    
     init();
 
 }
