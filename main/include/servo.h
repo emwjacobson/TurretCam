6cf4f6f4057256bc1f0ebab8851cfb7c0b51dd93
Emerson Jacobson <emwjacobson@gmail.com>
2022-05-21 11:21:04 -0700

    Added more servo functionality

--- main/include/servo.h
+++ main/include/servo.h
@@ -1,26 +1,20 @@
-#ifndef SERVO_H
-#define SERVO_H
+#ifndef SERVO__H
+#define SERVO__H
 
 #include "stdint.h"
 #include "esp_err.h"
+#include "driver/gpio.h"
 
 // Servo Information
 // 1ms pulse => Full Up
 // 1.5ms pulse => Center
 // 2ms pulse => Full Down
 #define PWM_PERIOD 20000 // 20000 us => 50 Hz => Servo PWM period
-#define PWM_MIN 1000
+#define PWM_MIN 700
 #define PWM_CENTER 1500
-#define PWM_MAX 2000
-#define PWM_PIN 99 // TODO: Set this pin number
-
-static const char* TAG = "Main";
-
-static const uint32_t duties = {
-    PWM_CENTER
-};
+#define PWM_MAX 2300
 
 esp_err_t init_servo();
-esp_err_t servo_set_rotation(uint32_t percent);
+esp_err_t servo_set_rotation(uint8_t percent);
 
 #endif
--- main/servo.c
+++ main/servo.c
@@ -1,19 +1,36 @@
 #include "servo.h"
-#include "pwm.h"
+#include "driver/pwm.h"
 #include "esp_log.h"
 #include "util.h"
 
+static const char* TAG_SERVO = "Servo";
+
+uint32_t pwm_duties = {
+    PWM_CENTER
+};
+
+uint32_t pwm_pins = {
+    5
+};
+
 esp_err_t init_servo() {
+    ESP_LOGI(TAG_SERVO, "Initializing Servo");
     esp_err_t err;
-    err = pwm_init(PWM_PERIOD, duties, 1, PWM_PIN);
+    err = pwm_init(PWM_PERIOD, &pwm_duties, 1, &pwm_pins);
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Error initializing PWM.");
+        ESP_LOGE(TAG_SERVO, "Error initializing PWM.");
+        return err;
+    }
+
+    err = pwm_set_phase(0, 0);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG_SERVO, "Error setting PWM phase.");
         return err;
     }
 
     err = pwm_start();
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Error starting PWM.");
+        ESP_LOGE(TAG_SERVO, "Error starting PWM.");
         return err;
     }
 
@@ -30,7 +47,9 @@ esp_err_t servo_set_rotation(uint8_t percent) {
     if (percent > 100) percent = 100;
 
     // Convert percent (0 to 100) to PWM range (1000 to 2000)
-    uint32_t duty = map(percent, 0, 100, PWM_MIN, PWM_MAX);
+    uint32_t duty = map(percent, 100, 0, PWM_MIN, PWM_MAX);
+
+    ESP_LOGI(TAG_SERVO, "Setting servo. Percent %i = %i duty", percent, duty);
 
     // TODO: This check is probably redundent
     if (duty > PWM_PERIOD) {
@@ -45,13 +64,13 @@ esp_err_t servo_set_rotation(uint8_t percent) {
     //       indexed of number of channel from pwm_init
     err = pwm_set_duty(0, duty);
     if (err != ESP_OK) {
-        ESP_LOGW(TAG, "Error setting PWM duty.");
+        ESP_LOGW(TAG_SERVO, "Error setting PWM duty.");
         return err;
     }
 
     err = pwm_start();
     if (err != ESP_OK) {
-        ESP_LOGW(TAG, "Error starting PWM.");
+        ESP_LOGW(TAG_SERVO, "Error starting PWM.");
         return err;
     }
 
